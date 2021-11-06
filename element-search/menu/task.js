8a2342abc612512997f236249dca4754f7ae5985
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-11-06 20:08:14 +0300

    Update

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,11 +3,14 @@ const menuLink = document.querySelectorAll(".menu__link");
 for (let i = 0; i < menuLink.length; i++) {
 	menuLink[i].onclick = function() {
 		//Найдите меню рядом со ссылкой. Если оно есть, переключите у него класс menu_active.
-		const menu = menuLink[i].closest(".menu__item");
-		const submenu = menu.querySelectorAll(".menu.menu_sub");
-		if (submenu) {
+		const menu = this.closest(".menu__item");
+		const submenu = menu.querySelector(".menu.menu_sub");
+		if (submenu.className === "menu menu_sub") {
 			submenu.className = "menu menu_sub menu_active";
 			//Запрещайте переход по ссылке для тех, что имеют вложенное меню. Остальные пункты меню должны без помех переводить пользователя на соответствующие страницы.
+			return false;
+		} else {
+			submenu.className = "menu menu_sub";
 			return false;
 		}
 	}
