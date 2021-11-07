28300358da7f3769cd976197e1351d11b31edaa8
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-11-07 12:37:15 +0300

    Initial commit

---
+++ dom/reveal/task.js
@@ -0,0 +1,17 @@
+const reveal = document.querySelectorAll(".reveal");
+const isInViewport = (element) => {
+	const viewportHeight = window.innerHeight;
+	const elementTop = element.getBoundingClientRect().top;
+	const elementBottom = element.getBoundingClientRect().bottom;
+	return elementTop < viewportHeight && elementBottom > 0 ? true : false;
+};
+for (let i = 0; i < reveal.length; i++) {
+	const revealAppearance = () => {
+		if (isInViewport(reveal[i])) {
+			reveal[i].classList.add("reveal_active"); 
+		} else {
+			reveal[i].classList.remove("reveal_active"); 
+		}
+	}
+	window.addEventListener('scroll', revealAppearance);
+}
