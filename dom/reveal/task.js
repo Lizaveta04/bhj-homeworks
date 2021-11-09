e025356ae1b92f380328ba864fddb87d6105e65f
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-11-09 20:13:15 +0300

    Solution

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,9 +1,11 @@
 const reveal = document.querySelectorAll(".reveal");
 const isInViewport = (element) => {
 	const viewportHeight = window.innerHeight;
-	const elementTop = element.getBoundingClientRect().top;
-	const elementBottom = element.getBoundingClientRect().bottom;
-	return elementTop < viewportHeight && elementBottom > 0 ? true : false;
+	//const elementTop = element.getBoundingClientRect().top;
+	//const elementBottom = element.getBoundingClientRect().bottom;
+	const { top, bottom } = element.getBoundingClientRect();
+	//return elementTop < viewportHeight && elementBottom > 0 ? true : false;
+	return top < viewportHeight && bottom > 0 ? true : false;
 };
 for (let i = 0; i < reveal.length; i++) {
 	const revealAppearance = () => {
