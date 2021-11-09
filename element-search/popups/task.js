28fa171732e03cbf0ea3fe0c68092ed6487ebcbd
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-11-09 21:48:53 +0300

    Solution

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -2,9 +2,7 @@ const modalMain = document.getElementById("modal_main");
 const modalClose = document.getElementsByClassName("modal__close");
 const showSuccess = document.getElementsByClassName("show-success");
 const modalSuccess = document.getElementById("modal_success");
-
 modalMain.className = "modal modal_active";
-
 for (let i = 0; i < modalClose.length; i++) {
 	modalClose[i].onclick = function() {
 		//Вариант "попроще":
@@ -16,7 +14,6 @@ for (let i = 0; i < modalClose.length; i++) {
 		modal.className = "modal modal__close";
 	}
 }
-
 showSuccess.item(0).onclick = function() {
 	modalMain.className = "modal modal__close";
 	modalSuccess.className = "modal modal_active";
