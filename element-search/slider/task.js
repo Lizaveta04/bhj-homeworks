f6eab84730bf5bd40c6f89f2331fc2c034c15f0f
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-11-10 15:37:42 +0300

    Update

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,31 +2,19 @@ const sliderItem = document.querySelectorAll(".slider__item");
 const sliders = Array.from(sliderItem);
 const prev = document.querySelector(".slider__arrow.slider__arrow_prev");
 const next = document.querySelector(".slider__arrow.slider__arrow_next");
-let slidersCount = sliders.length;
-let currentSlide = 0;
+
+let positionOfActiveSlide = sliders.findIndex((item, idx) => {
+	return item.className = "slider__item slider__item_active";
+})
 
 prev.onclick = function() {
-	if (sliders[slidersCount - 1].className === "slider__item slider__item_active") {
-		sliders[slidersCount - 1].className = "slider__item";
-		slidersCount --;
-		if (slidersCount === currentSlide) {
-			slidersCount = sliders.length;
-		}
-		sliders[slidersCount - 1].className = "slider__item slider__item_active";
-	} else {
-		sliders[slidersCount - 1].className = "slider__item slider__item_active";
-	}
+	sliders[positionOfActiveSlide].className = "slider__item";
+	positionOfActiveSlide < sliders.length-1 ? positionOfActiveSlide++ : positionOfActiveSlide = 0;
+	sliders[positionOfActiveSlide].className = "slider__item slider__item_active";
 }
 
 next.onclick = function() {
-	if (sliders[currentSlide].className === "slider__item slider__item_active") {
-		sliders[currentSlide].className = "slider__item";
-		currentSlide ++;
-		if (currentSlide === slidersCount) {
-			currentSlide = 0;
-		}
-		sliders[currentSlide].className = "slider__item slider__item_active";
-	} else {
-		sliders[currentSlide].className = "slider__item slider__item_active";
-	}
+	sliders[positionOfActiveSlide].className = "slider__item";
+	positionOfActiveSlide < sliders.length-1 ? positionOfActiveSlide++ : positionOfActiveSlide = 0;
+	sliders[positionOfActiveSlide].className = "slider__item slider__item_active";
 }
