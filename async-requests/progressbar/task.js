ec727c77bf43683c4068c1f065a63f3b0fada4d1
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-11-21 22:07:24 +0300

    Solution

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -6,8 +6,10 @@ const onClick = (e) => {
 	const form = document.getElementById("form");
 	const formData = new FormData(form);
 	const xhr = new XMLHttpRequest();
+	xhr.upload.onprogress = function(event) {
+		progress.value = event.loaded / event.total;
+	};
 	xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
-	//progress.value = 0.7;
 	xhr.send(formData);
 }
 button.addEventListener('click', onClick);
