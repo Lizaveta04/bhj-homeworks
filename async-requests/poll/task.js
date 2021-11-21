855e15e27a0102db660b763b65aa9f6576410083
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-11-21 21:48:34 +0300

    Solution

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -4,16 +4,12 @@ xhr.send();
 
 const pollTitle = document.getElementById("poll__title");
 const pollAnswers = document.getElementById("poll__answers");
+
 xhr.addEventListener('readystatechange', function() {
 	if (xhr.readyState === xhr.DONE && xhr.status === 200) {
 		pollTitle.innerText = JSON.parse(xhr.responseText).data.title;
 		let answers = JSON.parse(xhr.responseText).data.answers;
 		for (let i = 0; i < answers.length; i++) {
-			//pollAnswers.innerHTML =+ `
-			//<button class="poll__answer">
-			//	${answers[i]}
-            //</button>
-            //`;
             let html = `
 			<button class="poll__answer">
 				${answers[i]}
@@ -22,8 +18,9 @@ xhr.addEventListener('readystatechange', function() {
             pollAnswers.insertAdjacentHTML('beforeEnd', html);
             const buttons = document.querySelectorAll(".poll__answer");
         	for (let i = 0; i < buttons.length; i++) {
-            	buttons[i].onClick = function() {
+            	buttons[i].onclick = function() {
             		alert('Спасибо, ваш вопрос засчитан!');
+            		window.location.reload();
             	}
         	}
 		}
