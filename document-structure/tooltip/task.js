cd9e771af8f963e0bdeabdebd0248de18a84409d
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-11-26 21:22:26 +0300

    Update

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,20 +1,21 @@
 const hasTooltips = Array.from(document.querySelectorAll(".has-tooltip"));
+const tooltip = document.createElement("div");
+tooltip.classList.add("tooltip");
 
 for (let i = 0; i < hasTooltips.length; i++) {
+    hasTooltips[i].insertAdjacentElement('afterEnd', tooltip);
     const onClick = (e) => {
         e.preventDefault();
         const top = e.target.getBoundingClientRect().top;
         const left = e.target.getBoundingClientRect().left;
-        const html = `<div class="tooltip" style="left:${left}px; top:${top}px+20px">${e.target.title}</div>`;
-        hasTooltips[i].insertAdjacentHTML('afterEnd', html);
-        const tooltip = document.querySelector(".tooltip");
+        
         if (tooltip.innerText === e.target.title) {
             tooltip.classList.toggle("tooltip_active");
         } else {
+            tooltip.classList.add("tooltip_active");
             tooltip.innerText = e.target.title;
             tooltip.style.top = `${top + 20}px`; 
             tooltip.style.left = `${left}px`;
-            tooltip.classList.add("tooltip_active");
         } 
     }
     hasTooltips[i].addEventListener('click', onClick);
